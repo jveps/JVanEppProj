ed75399f2eb98075b74b19858d9ab7dc7d1e1003
J <jvanepp@wgu.edu>
2022-01-20 23:04:42 +0000

    Update

--- src/Controller/ModifyCustomerController.java
+++ src/Controller/ModifyCustomerController.java
@@ -4,11 +4,14 @@ import DAO.JDBC;
 import Model.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -42,13 +45,50 @@ public class ModifyCustomerController implements Initializable {
     private ComboBox<String> modCustCityComboBox;
 
     @FXML
-    void modCustCancelButtonPressed(ActionEvent event) {
-
+    void modCustCancelButtonPressed(ActionEvent event) throws IOException {
+        Stage stage;
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        Parent scene = FXMLLoader.load(getClass().getResource("/View/RecordOverview.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    void modCustOkButtonPressed(ActionEvent event) {
+    void modCustOkButtonPressed(ActionEvent event) throws IOException {
+        String newCustId = modCustIdField.getText();
+        String newCustName = modCustNameField.getText();
+        String newCustAddress = modCustAddyField.getText();
+        String newCustCountry = modCustCountryComboBox.getValue();
+        String newCustDivision = modCustCityComboBox.getValue();
+        String newCustPostalCode = modCustPostalCodeField.getText();
+        String newCustPhoneNum = modCustPhoneField.getText();
 
+        //There is currently a bug in this if statement. The default selection in the comboboxes are being detected as empty right now.
+        if (newCustName.trim().isEmpty()  || newCustAddress.trim().isEmpty() || modCustCityComboBox.getSelectionModel().isEmpty() || modCustCityComboBox.getSelectionModel().isEmpty() ||
+        newCustPostalCode.trim().isEmpty() || newCustPhoneNum.trim().isEmpty()){
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setTitle("ERROR");
+            a.setContentText("Please ensure all fields are filled");
+            a.showAndWait();
+        }
+
+        else{
+
+            Alert a = new Alert(Alert.AlertType.CONFIRMATION);
+            a.setTitle("Alert");
+            a.setContentText("Are you sure you want to update this customer?");
+            a.showAndWait();
+
+            if (a.getResult() == ButtonType.OK) {
+                JDBC.addCustomer(newCustId, newCustName, newCustAddress, newCustCountry, newCustDivision, newCustPostalCode, newCustPhoneNum);
+            }
+            //Open RecordOverview screen
+            Stage stage;
+            stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+            Parent scene = FXMLLoader.load(getClass().getResource("/View/RecordOverview.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+        };
     }
 
     @FXML
